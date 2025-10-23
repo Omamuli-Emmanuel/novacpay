c81cdb84f7b4b9a6afd0d7f1181516e93ffbe3ae
Omamuli-Emmanuel <51296637+Omamuli-Emmanuel@users.noreply.github.com>
2025-10-23 22:22:24 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,9 @@ pluginManagement {
     }
 }
 
+// Remove dependencyResolutionManagement entirely or use this:
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
 }
 
 rootProject.name = "NovacPaymentSDK"
