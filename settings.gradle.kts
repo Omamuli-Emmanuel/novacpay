9abb932e6fdeffd4a7f9fd6316d77c98406b52d1
Omamuli-Emmanuel <51296637+Omamuli-Emmanuel@users.noreply.github.com>
2025-10-23 22:24:50 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,13 @@ pluginManagement {
     }
 }
 
-// Remove dependencyResolutionManagement entirely or use this:
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 
 rootProject.name = "NovacPaymentSDK"
