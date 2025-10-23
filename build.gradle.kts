fa493169691c3e94f799b5070f7087d83aaf2af9
Omamuli-Emmanuel <51296637+Omamuli-Emmanuel@users.noreply.github.com>
2025-10-23 21:03:49 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,23 +21,3 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
-
-// Configure publishing for all library subprojects
-subprojects {
-    plugins.withId("com.android.library") {
-        apply(plugin = "maven-publish")
-        
-        afterEvaluate {
-            publishing {
-                publications {
-                    create<MavenPublication>("release") {
-                        from(components["release"])
-                        groupId = "com.github.Omamuli-Emmanuel"
-                        artifactId = "novacpay"
-                        version = "1.0.0"
-                    }
-                }
-            }
-        }
-    }
-}
