99f61a273e235d1b26e8333be041741f36505ef1
Omamuli-Emmanuel <51296637+Omamuli-Emmanuel@users.noreply.github.com>
2025-10-24 19:25:42 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,10 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+// Add root-level publishToMavenLocal task
+tasks.register("publishToMavenLocal") {
+    dependsOn(":lib:publishToMavenLocal")
+    group = "publishing"
+    description = "Publishes all Maven publications to the local Maven repository."
+}
